6a8a2e9da6cdfaba5df620a685aba4acbd7ae3f3
chinmayzhere <140723464+chinmayzhere@users.noreply.github.com>
2024-03-13 19:53:44 +0530

    Create awsusage.sh

---
+++ awsusage.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+#############################################
+#Author: Chinmay
+#Date : 13 March, 2024
+#Version: V1
+
+#This script will report the AWS resource usage.
+
+################################################
+
+
+#We are planning to track AWS S3, AWS EC2, AWS Lambda and AWS IAM users
+
+
+#For AWS S3:
+#list all S3 buckets
+set -x #debug mode
+
+echo "The buckets in your AWS account include:"
+aws s3 ls
+
+#list EC2 instances
+
+echo "The EC2 instances in your AWS account include:"
+
+#aws ec2 describe-instances
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+#list AWS Lambda functions
+
+#aws lambda list-functions
+
+
+#list AWS IAM users
+
+#aws iam list-users
+
+
+
+
+
+
+
